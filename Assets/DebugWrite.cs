c7e53d3fd6a9f11d3d9b1d844b9b6206974c7f9d
oja89 <56597978+oja89@users.noreply.github.com>
2021-03-17 10:58:26 +0200

    Change hands

---
+++ Assets/DebugWrite.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugWrite : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log ("Start DEBUG");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log ("Update DEBUG");
+    }
+	public static void WriteLog() 
+	{
+		Debug.Log ("WriteLog");
+	}
+}
