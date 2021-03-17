eb2c7545749a06cb63ebc790f0bb7a0e2e81d379
oja89 <56597978+oja89@users.noreply.github.com>
2021-03-17 09:28:28 +0200

    Minor changes

---
+++ Assets/ObjectManipulation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+
+
+
+public class ObjectManipulation : MonoBehaviour {
+	
+	[SerializeField]	
+	GameObject objectToDestroy;
+
+	
+	//Called when Button is clicked
+	public static void debugPress() {
+	Debug.Log("Pressed");
+	
+	}
+}
